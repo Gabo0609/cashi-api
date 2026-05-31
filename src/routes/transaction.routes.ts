fc793bc5d3d0b51ca22fa389ff171d0d83c25f0e
Gabo <TU_CORREO_DE_GITHUB>
2026-05-31 19:07:47 -0400

    refactor: simplify receipt upload route

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { serveStatic } from "@hono/node-server/serve-static";
 import {
   createTransaction,
   deleteTransaction,
@@ -9,30 +8,11 @@ import {
   updateTransaction,
   uploadReceipt,
 } from "../controllers/transaction.controller.js";
-import { upload } from "../middlewares/upload.middleware.js";
 
 export const transactionRoutes = new Hono();
 
 transactionRoutes.get("/balance", getBalance);
-transactionRoutes.post(
-  "/upload",
-  async (c, next) => {
-    await new Promise<void>((resolve, reject) => {
-      upload.single("receipt")(
-        c.req.raw as any,
-        {} as any,
-        (err: any) => {
-          if (err) reject(err);
-          else resolve();
-        }
-      );
-    });
-
-    await next();
-  },
-  uploadReceipt
-);
-
+transactionRoutes.post("/upload", uploadReceipt);
 transactionRoutes.get("/", getTransactions);
 transactionRoutes.get("/:id", getTransactionById);
 transactionRoutes.post("/", createTransaction);
